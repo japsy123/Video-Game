d1696095e2a7c851b76ff30f8aa650ae0b09b8cb
Jal Panchal <jal08panchal@gmail.com>
2019-05-19 08:35:59 -0700

    Setting up Components

---
+++ src/AddUser.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import User from "./User";
+
+export default class AddUser extends Component {
+  render() {
+    return (
+      <div>
+        AdddUser
+        <User />
+      </div>
+    );
+  }
+}
